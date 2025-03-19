c9f6c9a13f115b4cf16108a604073b563f6463ad
stinta <kyrbynyt@gmail.com>
2025-03-19 21:46:17 +0200

    add one more maybe needed particle system function

--- src/particles.c
+++ src/particles.c
@@ -1,10 +1,31 @@
 #include "particles.h"
 #include "ecs.h"
 #include "raylib.h"
+#include "raymath.h"
 #include "timing_utilities.h"
 
 void particles_push(Particles *particles, Particle p) {
     particles->particles[(particles->count++) % MAX_PARTICLES] = p;
+}
+
+void particles_spawn_n_in_dir(Particles *particles, int n, Color c, Vector2 dir, Vector2 pos) {
+    Particle base_particle = {
+        .created_at = GetTime(),
+        .active = true,
+        .color = c,
+        .transform = TRANSFORM(pos.x, pos.y, 16, 16),
+        .physics = DEFAULT_PHYSICS(),
+    };
+    for (int i = 0; i < n; i++) {
+        const Vector2 unique_rotation = Vector2Rotate(dir, GetRandomValue(-30 * DEG2RAD, 30 * DEG2RAD));
+        const Color unique_color = ColorBrightness(c, GetRandomValue(0, 20) / 10.0);
+        particles_push(particles, (Particle){.created_at = base_particle.created_at + GetRandomValue(0, 10) / 10.0,
+                                             .physics = (PhysicsComp){.grounded = false,
+                                                                      .velocity = Vector2Scale(unique_rotation,
+                                                                                               PARTICLE_VELOCITY)},
+                                             .color = unique_color,
+                                             .transform = base_particle.transform});
+    }
 }
 void particles_draw(const Particles *particles) {
     for (size_t i = 0; i < MAX_PARTICLES; i++) {
--- src/particles.h
+++ src/particles.h
@@ -6,6 +6,7 @@
 
 #define PARTICLE_LIFETIME 2.0
 #define MAX_PARTICLES 500
+#define PARTICLE_VELOCITY 500
 
 typedef struct {
     TransformComp transform;
@@ -21,6 +22,7 @@ typedef struct {
 } Particles;
 
 void particles_push(Particles* particles, Particle p);
+void particles_spawn_n_in_dir(Particles *particles, int n, Color c, Vector2 dir, Vector2 pos);
 void particles_draw(const Particles *particles);
 void particles_update(Particles *particles, const Stage *stage, float dt);
 
