72af6f06adfd0f88b904949f2783ff554eacb591
stinta <kyrbynyt@gmail.com>
2025-03-21 17:56:52 +0200

    definitions

---
+++ src/weapon.c
@@ -0,0 +1,24 @@
+#include "weapon.h"
+#include <raylib.h>
+#include <raymath.h>
+
+void pistol_try_shoot(struct Weapon *this, Bullets *bullets, const TransformComp *from, const Camera2D *camera) {
+    const Vector2 mouse_pos = GetScreenToWorld2D(GetMousePosition(), *camera);
+    const Vector2 dir = Vector2Normalize(Vector2Subtract(mouse_pos, transform_center(from)));
+    bullets_spawn_bullet(from, bullets, dir, PURPLE);
+    this->pistol.last_shot = GetTime();
+    PlaySound(this->pistol.shoot_sound);
+}
+
+Weapon create_pistol(double file_rate) {
+    return (Weapon){
+        .type = WT_PISTOL,
+        .pistol =
+            {
+                .last_shot = 0.0,
+                .fire_rate = file_rate,
+                .shoot_sound = LoadSound("assets/sfx/pistol_shoot.wav"),
+                .try_shoot = pistol_try_shoot,
+            },
+    };
+}
---
+++ src/weapon.h
@@ -0,0 +1,26 @@
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include "bullet.h"
+#include "ecs.h"
+typedef enum {
+    WT_PISTOL
+} WeaponType;
+
+typedef struct Weapon{
+    WeaponType type;
+    union {
+        struct {
+            double last_shot;
+            double fire_rate;
+            Sound shoot_sound;
+            void (*try_shoot)(struct Weapon* this, Bullets* bullets, const TransformComp* from, const Camera2D* camera);
+        } pistol;
+    };
+} Weapon;
+
+void pistol_try_shoot(struct Weapon* this, Bullets* bullets, const TransformComp* from, const Camera2D* camera);
+
+Weapon create_pistol(double file_rate);
+
+#endif
